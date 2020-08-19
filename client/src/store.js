b625339dd1bfb1ba0290f68f5ee1e84ff4719604
Abayomi Aishida <yomicorperate@gmail.com>
2020-08-19 08:44:12 +0100

    Created redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootRudcer from "./reducers";
+
+const initialState = {};
+
+const middlewre = { thunk };
+
+const store = createStore(
+  rootRudcer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewre))
+);
+
+export default store;
