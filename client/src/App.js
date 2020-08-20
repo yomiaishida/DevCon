e76ac09c88eba9fefb669b3f7737f2c071e20645
Abayomi Aishida <yomicorperate@gmail.com>
2020-08-20 09:11:50 +0100

    changed path name

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import Alert from "./components/layouts/Alert";
+import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
