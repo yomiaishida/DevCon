51f8a27c03441cf17fee45805c6df0518136797c
Abayomi Aishida <yomicorperate@gmail.com>
2020-12-09 20:59:11 +0100

    Addressing console warnings

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -45,8 +45,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? "" : profile.social.youtube,
       instagram: loading || !profile.social ? "" : profile.social.instagram,
     });
-    // eslint-disable-next-line
-  }, [loading]);
+  }, [loading, getCurrentProfile]);
 
   const {
     company,
