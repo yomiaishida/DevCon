4cc629df41980adba3bacb06323add4efc0d5df9
Abayomi Aishida <yomicorperate@gmail.com>
2020-12-22 03:57:35 +0100

    Created component that gets github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import { Link } from "react-router-dom";
 
 const Profile = ({
@@ -35,7 +36,7 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
-          <div class="profile-grid my-1">
+          <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
             <div className="profile-exp bg-white p-2">
@@ -68,6 +69,9 @@ const Profile = ({
                 <h4>No education credentials </h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
