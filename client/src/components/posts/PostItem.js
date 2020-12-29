2cd92f59d6755cf999156074c9f1672da850c0a7
Abayomi Aishida <yomicorperate@gmail.com>
2020-12-29 08:28:04 +0100

    Finished up postitem component

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -6,40 +6,38 @@ import { connect } from "react-redux";
 
 const PostItem = ({
   auth,
-  post: { _id, text, name, abatar, user, likes, comments, date },
+  post: { _id, text, name, avatar, user, likes, comments, date },
 }) => (
   <div class="post bg-white p-1 my-1">
     <div>
       <a href="profile.html">
-        <img
-          class="round-img"
-          src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-          alt=""
-        />
-        <h4>John Doe</h4>
+        <img class="round-img" src={avatar} alt="" />
+        <h4>{name}</h4>
       </a>
     </div>
     <div>
-      <p class="my-1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-        corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis. Amet
-        accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-        eveniet cum cupiditate aliquam?
+      <p class="my-1">{text}</p>
+      <p class="post-date">
+        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
-      <p class="post-date">Posted on 04/16/2019</p>
       <button type="button" class="btn btn-light">
-        <i class="fas fa-thumbs-up"></i>
-        <span>4</span>
+        <i class="fas fa-thumbs-up"></i>{" "}
+        <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
       <button type="button" class="btn btn-light">
         <i class="fas fa-thumbs-down"></i>
       </button>
-      <a href="post.html" class="btn btn-primary">
-        Discussion <span class="comment-count">2</span>
-      </a>
-      <button type="button" class="btn btn-danger">
-        <i class="fas fa-times"></i>
-      </button>
+      <Link to={`/post/${_id}`} class="btn btn-primary">
+        Discussion{" "}
+        {comments.length > 0 && (
+          <span class="comment-count">{comments.length}</span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type="button" class="btn btn-danger">
+          <i class="fas fa-times"></i>
+        </button>
+      )}
     </div>
   </div>
 );
